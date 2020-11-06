3122c6013e89ec67c1f66845347d2cabbd4761c7
Eli Cusic <tntitans31@gmail.com>
2020-11-06 15:37:56 -0600

    created new user template - beginning refactor process to DRY up controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :first_name, :last_name, :email, presence: true
+    validates :first_name, :last_name, :email, :type, presence: true
     validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "only allows letters with @" }
     has_secure_password
 
